14f80cf49a4e5d94cdadea6447af3f2780f1180b
Rafael Lacerda <rlag@outlook.com>
2024-11-19 12:26:55 -0300

    Commit inicial

---
+++ src/main/java/br/com/rlag/alfabetiza/user_answer_service/UserAnswerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rlag.alfabetiza.user_answer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAnswerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAnswerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rlag/alfabetiza/user_answer_service/UserAnswerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rlag.alfabetiza.user_answer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAnswerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
